1cf9d002eee5eb18e93e66b5a08be004dc06da5d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-02-10 22:48:00 -0500

    ADD: se realizo correctamente la creacion de la api

--- src/Helpers/wsStockPrecor.php
+++ src/Helpers/wsStockPrecor.php
@@ -25,10 +25,10 @@ class WsStockPrecor extends SoapHeader
     }
 }
 
-
-function getServiceWsStosk(): SoapClient
+function precor_getServiceClient(): SoapClient
 {
-    $wsdl = "http://srvpmpsapqas.precor.com.pe:8002/sap/bc/srt/wsdl/bndg_EC860AAA970429F180C900505601055A/wsdl11/allinone/ws_policy/document?sap-client=400";
+    // $wsdl = "http://192.168.50.132:8002/sap/bc/srt/wsdl/bndg_EC860AAA970429F180C900505601055A/wsdl11/allinone/ws_policy/document?sap-client=400?wsdl";
+    $wsdl =  assetWebservices("webstock.xml");
     $username = "EXT_NOVA";
     $password = "Novategica2$";
 
@@ -45,4 +45,35 @@ function getServiceWsStosk(): SoapClient
     $client = new SoapClient($wsdl, $options);
     // $client->__setSoapHeaders(array($wsse_header));
     return $client;
+}
+
+function precor_getStockBySkuAndIdSoc($sku, $id_soc)
+{
+    try {
+        $ws = precor_getServiceClient();
+
+        $response = $ws->ZsdpefEcom01(
+            array(
+                "IdMat" =>   intval($sku),
+                "IdSoc" => $id_soc
+            )
+        );
+        $object = new stdClass();
+        $object->status = true;
+        if ($response) {
+            $stock = doubleval($response->StockWeb) ?? 0;
+            $object->stock = $stock;
+            $object->sku = $sku;
+            $object->id_soc = $id_soc;
+            $object->msg = "OK";
+            return $object;
+        }
+        $object->status = false;
+        return $object;
+    } catch (\Throwable $th) {
+        $object = new stdClass();
+        $object->status = false;
+        $object->msg = $th->getMessage();
+        return $object;
+    }
 }
--- src/WsStockController.php
+++ src/WsStockController.php
@@ -10,22 +10,15 @@ class WsStockController
 
     public static function getStockPrecor()
     {
+        // example:
+        // http://precor.test/wp-json/ws_precor/v1/getStock?sku=432139&id_soc=PR01
         register_rest_route('ws_precor/v1', '/getStock', array(
             'methods' => 'GET',
-            'callback' => function () {
-                try {
-                    $ws = getServiceWsStosk();
-                    $response = $ws->ZsdpefEcom01(
-                        array(
-                            "IdMat" =>  array("_" => 432139),
-                            "IdSoc" => array("_" => "MA01"),
-                        )
-                    );
-                    return ($response);
-                } catch (\Throwable $th) {
-                    return $th;
-                }
-            },
+            'callback' => function ($params) {
+                $params = $params->get_params();
+                $response = precor_getStockBySkuAndIdSoc(intval($params["sku"]), $params["id_soc"]);
+                return ($response);
+            }
         ));
     }
 }
