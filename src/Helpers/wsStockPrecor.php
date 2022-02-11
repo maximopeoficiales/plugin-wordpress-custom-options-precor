d1f555d61453258c68beb2390aab4d2a7fc1de03
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-02-10 22:49:19 -0500

    ADD: se elimino clase que no se estaba utilizando para obtener el stock de los productos

--- src/Helpers/wsStockPrecor.php
+++ src/Helpers/wsStockPrecor.php
@@ -1,30 +1,4 @@
 <?php
-class WsStockPrecor extends SoapHeader
-{
-    private $wss_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd';
-
-    public function __construct($user, $pass, $ns = null)
-    {
-
-        $auth = new stdClass();
-        $auth->Username = new SoapVar($user, XSD_STRING, NULL, $this->wss_ns, NULL, $this->wss_ns);
-        $auth->Password = new SoapVar($pass, XSD_STRING, NULL, $this->wss_ns, NULL, $this->wss_ns);
-
-        $username_token = new stdClass();
-        $username_token->UsernameToken = new SoapVar($auth, SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'UsernameToken', $this->wss_ns);
-
-        $security_sv = new SoapVar(
-            new SoapVar($username_token, SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'UsernameToken', $this->wss_ns),
-            SOAP_ENC_OBJECT,
-            NULL,
-            $this->wss_ns,
-            'Security',
-            $this->wss_ns
-        );
-        parent::__construct($this->wss_ns, 'Security', $security_sv, true);
-    }
-}
-
 function precor_getServiceClient(): SoapClient
 {
     // $wsdl = "http://192.168.50.132:8002/sap/bc/srt/wsdl/bndg_EC860AAA970429F180C900505601055A/wsdl11/allinone/ws_policy/document?sap-client=400?wsdl";
@@ -32,7 +6,6 @@ function precor_getServiceClient(): SoapClient
     $username = "EXT_NOVA";
     $password = "Novategica2$";
 
-    // $wsse_header = new WsStockPrecor($username, $password);
     $options = array(
         'trace' => true,
         'login' => $username,
